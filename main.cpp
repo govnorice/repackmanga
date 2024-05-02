d30f786956fc9a7184464ed945104e287ddc7089
progozone122 <80642969+progzone122@users.noreply.github.com>
2024-05-03 00:11:57 +0300

    add CLI mode

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <string>
 #include "modules/repack.h"
 
-int main() {
-    Repack::start("./a");
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if (argc >= 3) {
+        string option1 = argv[1];
+        string option2 = argv[2];
+
+        if (option1 == "--path") {
+            Repack::start(option2);
+        }
+    } else if (argc == 1) {
+        cout << "GUI" << endl;
+    } else {
+        cerr << "Error: Insufficient arguments" << endl;
+        return 1;
+    }
     return 0;
 }
