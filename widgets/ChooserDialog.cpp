cead554ba1cc169b06075e0373aedf03ecafc80e
progozone122 <80642969+progzone122@users.noreply.github.com>
2024-05-04 00:27:03 +0300

    add ChooserDialog widget

---
+++ widgets/ChooserDialog.cpp
@@ -0,0 +1,28 @@
+#include "ChooserDialog.h"
+
+void ChooserDialog::init(GtkFileChooserAction action, string title, string cancel_name, string accept_name) {
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), title.c_str());
+
+    dialog = gtk_file_chooser_dialog_new(title.c_str(),
+                                         GTK_WINDOW(window),
+                                         action,
+                                         cancel_name.c_str(),
+                                         GTK_RESPONSE_CANCEL,
+                                         accept_name.c_str(),
+                                         GTK_RESPONSE_ACCEPT,
+                                         NULL);
+}
+string ChooserDialog::run() {
+    string path;
+    res = gtk_dialog_run(GTK_DIALOG(dialog));
+    if (res == GTK_RESPONSE_ACCEPT) {
+        GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
+        path = gtk_file_chooser_get_filename(chooser);
+        cout << path << endl;
+    }
+
+    gtk_widget_destroy(dialog);
+
+    return path;
+}
---
+++ widgets/ChooserDialog.h
@@ -0,0 +1,20 @@
+#ifndef REPACKMANGA_CHOOSERDIALOG_H
+#define REPACKMANGA_CHOOSERDIALOG_H
+
+#include <gtk/gtk.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class ChooserDialog {
+private:
+    GtkWidget *dialog, *window;
+    gint res;
+public:
+    void init(GtkFileChooserAction action, string title, string cancel_name, string accept_name);
+    string run();
+};
+
+
+#endif
